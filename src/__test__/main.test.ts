40e4cb48317b806f6296a625f1a96f352d219b5f
readyp <readypamungkas@gmail.com>
2024-07-23 08:57:17 +0700

    done

--- src/__test__/main.test.ts
+++ src/__test__/main.test.ts
@@ -1,7 +1,30 @@
 import { describe, test, expect } from 'vitest'
+import { sortNumber, testNomor1, testNomor2, testNomor3 } from '../main'
 
 describe('Main Suite Test', () => {
   test('Sanity Check', () => {
     expect(1 + 1).toBe(2)
+  })
+
+  test('fn sortNumber should return correct value', () => {
+    expect(sortNumber([5, 2, 9, 1, 5, 6])).toEqual([9, 6, 5, 5, 2, 1])
+  })
+
+  test('fn testNomor1 should return correct value', () => {
+    expect(testNomor1([1, 12, 31, 5, 3, 23, 4, 5, 22])).toBe(23)
+    expect(testNomor1([-0.5, -0.76, 0.45, -0.2, 4.5, 3.5])).toBe(3.5)
+    expect(testNomor1([98, 12, 42, 13, 13, 56, 100, 99])).toBe(99)
+  })
+
+  test('fn testNomor2 should return correct value', () => {
+    expect(testNomor2(['A', 'B', 'C', 'B', 'A'])).toBe('B')
+    expect(testNomor2(['A', 'B', 'C', 'D', 'E', 'C', 'F', 'Z'])).toBe('C')
+    expect(testNomor2(['A', 'B', 'C', 'X', 'Y', 'Z'])).toBeFalsy()
+  })
+
+  test('fn testNomor3 should return correct value', () => {
+    expect(testNomor3([1, 2, 4, 4, 5, 6, 7, 7, 8, 8], 12)).toBeTruthy()
+    expect(testNomor3([1, 2, 4, 4, 5, 8, 9, 9, 12, 19], 4)).toBeFalsy()
+    expect(testNomor3([-9.3, -0.5, 0.25, 0.3, 1.34], -7.96)).toBeTruthy()
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,57 @@
-console.log('Hello World')
+// utility fn
+export const sortNumber = (arr: number[]): number[] => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        const temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+
+  return arr
+}
+
+export const testNomor1 = (arr: number[]): number | null => {
+  if (arr.length < 2) {
+    return null
+  }
+
+  const sortedNumber = sortNumber(arr)
+  return sortedNumber[1]
+}
+
+export const testNomor2 = (arr: string[]): string | boolean => {
+  const recurringObject: Record<string, number> = {}
+
+  for (let char of arr) {
+    if (recurringObject[char]) {
+      return char
+    }
+
+    recurringObject[char] = (recurringObject[char] ?? 0) + 1
+  }
+
+  return false
+}
+
+export const testNomor3 = (arr: number[], arg: number): boolean => {
+  let left = 0
+  let rigth = arr.length - 1
+  let totalSum = 0
+
+  while (left < rigth) {
+    totalSum = arr[left] + arr[rigth]
+
+    if (Math.abs(totalSum - arg) < 1e-10) {
+      return true
+    } else if (totalSum > arg) {
+      rigth--
+    } else {
+      left++
+    }
+  }
+
+  return false
+}
